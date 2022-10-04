761ec535fc731235f3e0dc4a8b302f0341df8f37
becca <beccahsiung@gmail.com>
2022-10-04 09:50:03 -0700

    added /bin/ to the path

--- sshell.c
+++ sshell.c
@@ -10,6 +10,14 @@
 
 
 // }
+char* cmd_convert(char* command){
+        /* add /bin/ to command */
+        printf("got in");
+        char* final_cmd = strcat("/bin/", command);
+        printf("got concat");
+        printf("final command: '%s'", final_cmd);
+        return final_cmd;
+}
 int main(void)
 {
         char cmd[CMDLINE_MAX];
@@ -18,6 +26,7 @@ int main(void)
                 char *nl;
                 //int retval;
                 pid_t pid;
+                //char path = "";
                 char *args[] = {"Hi", NULL};
 
 
@@ -36,33 +45,45 @@ int main(void)
 
                 /* Remove trailing newline from command line */
                 nl = strchr(cmd, '\n');
-                if (nl)
+                if (nl){
                         *nl = '\0';
+                }
 
                 /* Builtin command */
                 if (!strcmp(cmd, "exit")) {
                         fprintf(stderr, "Bye...\n");
                         break;
                 }
+                //cmd_convert(cmd);
 
                 /* Regular command */
                 // retval = system(cmd);
                 // fprintf(stdout, "Return status value for '%s': %d\n",
                 //         cmd, retval);
 
+                /* add "/bin/" to command */
+                //char *final_cmd;
+                //char src[] = "/bin/";
+                char bin[CMDLINE_MAX] = "/bin/";
+                strcat(bin, cmd);
+                printf("%s",bin);
+                //printf("final command: '%s'", src);
+        
+
+
                 /* fork() + exec() + wait() */
                 pid = fork();
                 if (pid == 0) {
                         /* Child */
-                        printf("cmd: '%s'", cmd);
-                        execv(cmd,args);
+                        //printf("cmd: '%s'", cmd);
+                        execv(bin,args);
                         perror("execv");
                         exit(1);
                 } else if (pid > 0) {
                         /* Parent */
                         int status;
                         waitpid(pid, &status, 0);
-                        printf( "Return status value for '%s' : %d\n", cmd, WEXITSTATUS(status));
+                        printf( "Return status value for '%s' : %d\n", bin, WEXITSTATUS(status));
                 } else {
                         perror("fork");
                         exit(1);
