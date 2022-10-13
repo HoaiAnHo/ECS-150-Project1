8021d88095d3be1b543aee3605b1d1185274fdff
becca <beccahsiung@gmail.com>
2022-10-13 13:45:15 -0700

    update headers

--- sshell.c
+++ sshell.c
@@ -1,8 +1,11 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
-#include <fcntl.h>
+
 
 #define CMDLINE_MAX 512
 #define SIZE 500
